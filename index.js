d72bfaf30a5cd4d9b1661acb9d19b104be8e57cf
Nataliya R <n.rudenko@gmail.com>
2019-09-18 02:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const produceDrivingRange = function(blockRange) {
   return function(start, end) {
-    const distance = Math.abs(parseInt(start) - paseInt(end));
+    const distance = Math.abs();
     const difference = blockRange - distance;
     if (difference >= 0) {
       return `within range by ${difference}`;
